0b7517756dc1c3df4efd02b51b851f7f5dd98153
TOGoS <togos00@gmail.com>
2013-09-09 22:35:49 -0500

    Various updates to the ant simulator.

--- src/togos/vizations/Ants.java
+++ src/togos/vizations/Ants.java
@@ -7,10 +7,15 @@ import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 public class Ants
 {
+	static Random r = new Random();
+		
 	interface EntityCheck {
 		public boolean apply( Entity e );
 	}
@@ -29,6 +34,7 @@ public class Ants
 		}
 	}
 	
+	static int feedings = 0;
 	static int[] lookForDX = new int[] {-1, 0, 1, 1, 1, 0, -1, -1};
 	static int[] lookForDY = new int[] {-1, -1, -1, 0, 1, 1, 1, 0};
 	
@@ -38,8 +44,6 @@ public class Ants
 		public ActiveEntity( Color c ) {
 	        super(c);
         }
-		
-		static Random r = new Random();
 		
 		protected boolean lookFor( Map m, int x, int y, EntityCheck target ) {
 			int init = r.nextInt(8);
@@ -129,14 +133,15 @@ public class Ants
 	static final Entity ANT_WITH_FOOD = new ActiveEntity(new Color(0.3f, 0, 0)) {
 		@Override public void update( Map m, int x, int y ) {
 			if( lookFor(m, x, y, IS_FEEDEE) ) {
+				++feedings;
 				m.put( targetX, targetY, FED_FEEDEE );
-				m.put( x, y, ANT_EXITED_ABOUT_JOB );
+				m.put( x, y, EMPTY_ANT_LEAVING_FEEDEE_MARKER );
 			} else if( (r.nextInt(10) < 8 && lookFor(m, x, y, IS_NEAR_FEEDEE)) || lookFor(m, x, y, IS_WALKABLE) ) {
 				walkThere( m, x, y );
 			}
 		}
 	};
-	static final Entity EXCITED_ANT_WITH_FOOD = new ActiveEntity(new Color(0.3f, 0, 0)) {
+	static final Entity ANT_WITH_FOOD_LEAVING_FOOD_MARKER = new ActiveEntity(new Color(0.3f, 0, 0)) {
 		@Override public void update( Map m, int x, int y ) {
 			Entity newSelf = r.nextInt(15) < 1 ? ANT_WITH_FOOD : this;
 			if( lookFor(m, x, y, IS_FEEDEE) ) {
@@ -144,20 +149,20 @@ public class Ants
 				while( lookFor(m, x, y, IS_UNMARKED_GRASS) ) {
 					putAtTarget(m, GRASS_WITH_FEEDEE_MARKER);
 				}
-				m.put( x, y, ANT_EXITED_ABOUT_JOB );
+				m.put( x, y, EMPTY_ANT_LEAVING_FEEDEE_MARKER );
 			} else if( (r.nextInt(10) < 8 && lookFor(m, x, y, IS_NEAR_FEEDEE)) || lookFor(m, x, y, IS_WALKABLE) ) {
 				walkThereAndBecomeLeaving(m, x, y, newSelf, GRASS_WITH_FOOD_MARKER);
 			}
 		}
 	};
-	static final Entity ANT_EXITED_ABOUT_JOB = new ActiveEntity(new Color(0.3f, 0, 0)) {
+	static final Entity EMPTY_ANT_LEAVING_FEEDEE_MARKER = new ActiveEntity(new Color(0.3f, 0, 0)) {
 		@Override public void update( Map m, int x, int y ) {
 			Entity newSelf = r.nextInt(15) < 1 ? ANT : this;
 			if( lookFor(m, x, y, IS_FOOD)) {
 				m.put(targetX, targetY, GRASS_WITH_FOOD_MARKER);
-				m.put(x, y, EXCITED_ANT_WITH_FOOD);
+				m.put(x, y, ANT_WITH_FOOD_LEAVING_FOOD_MARKER);
 			} else if( (r.nextInt(10) < 8 && lookFor(m, x, y, IS_NEAR_FOOD)) ) {
-				walkThereAndBecome( m, x, y, newSelf );
+				walkThereAndBecomeLeaving( m, x, y, newSelf, GRASS_WITH_FEEDEE_MARKER );
 			} else if( lookFor(m, x, y, IS_WALKABLE) ) {
 				walkThereAndBecomeLeaving( m, x, y, newSelf, GRASS_WITH_FEEDEE_MARKER );
 			}
@@ -167,7 +172,11 @@ public class Ants
 		@Override public void update( Map m, int x, int y ) {
 			if( lookFor(m, x, y, IS_FOOD)) {
 				m.put(targetX, targetY, GRASS_WITH_FOOD_MARKER);
-				m.put(x, y, EXCITED_ANT_WITH_FOOD);
+				m.put(x, y, ANT_WITH_FOOD_LEAVING_FOOD_MARKER);
+			} else if( lookFor(m, x, y, IS_FEEDEE) ) {
+				if( lookFor(m, x, y, IS_WALKABLE) ) {
+					walkThereAndBecomeLeaving( m, x, y, EMPTY_ANT_LEAVING_FEEDEE_MARKER, GRASS_WITH_FEEDEE_MARKER );
+				}
 			} else if( (r.nextInt(10) < 8 && lookFor(m, x, y, IS_NEAR_FOOD)) || lookFor(m, x, y, IS_WALKABLE) ) {
 				walkThere( m, x, y );
 			}
@@ -182,9 +191,7 @@ public class Ants
 			this.w = w;
 			this.h = h;
 			this.data = new Entity[w*h];
-			for( int i=0; i<data.length; ++i ) {
-				data[i] = GRASS;
-			}
+			fill(GRASS);
 		}
 		
 		public Entity get( int x, int y ) {
@@ -198,45 +205,81 @@ public class Ants
 			y = MathUtil.fdmod(y, h);
 			data[w*y+x] = dat;
 		}
+
+		public void fill(Entity filler) {
+			for( int i=0; i<data.length; ++i ) {
+				data[i] = filler;
+			}
+		}
 	}
 	
-	static void draw( Map m, Graphics g ) {
+	static void draw( Map m, Graphics g, int scale ) {
 		for( int y=0; y<m.h; ++y ) for( int x=0; x<m.w; ++x ) {
 			g.setColor(m.data[m.w*y+x].getColor());
-			g.fillRect(x*4, y*4, 4, 4);
+			g.fillRect(x*scale, y*scale, scale, scale);
 		}
 	}
 	
-	Random r = new Random();
 	final Map map = new Map( 640/4, 360/4 );
 	
-	void initMap() {
-		int rx = r.nextInt(map.w);
-		int ry = r.nextInt(map.h);
-		for( int i=0; i<16384; ++i ) {
-			map.put(rx, ry, WATER);
-			switch( r.nextInt(4) ) {
-			case 0: ++rx; break;
-			case 1: --rx; break;
-			case 2: ++ry; break;
-			case 3: --ry; break;
+	void initMap(int seed) {
+		map.fill( GRASS );
+		
+		if( (seed % 4) < 3 ) { 
+			// Add oceans!
+			int rx = r.nextInt(map.w);
+			int ry = r.nextInt(map.h);
+			for( int i=0; i<16384; ++i ) {
+				map.put(rx, ry, WATER);
+				switch( r.nextInt(4) ) {
+				case 0: ++rx; break;
+				case 1: --rx; break;
+				case 2: ++ry; break;
+				case 3: --ry; break;
+				}
 			}
 		}
 		
-		for( int i=0; i<256; ++i ) {
-			Entity put;
-			switch( r.nextInt(3) ) {
-			case 0: put = ANT; break;
-			default: put = FEEDEE; break;
+		if( (seed % 2) == 0 ) {
+			// Scatter ants and receptacles
+			for( int i=0; i<256; ++i ) {
+				Entity put;
+				switch( r.nextInt(3) ) {
+				case 0: put = ANT; break;
+				default: put = FEEDEE; break;
+				}
+				map.put( r.nextInt(map.w), r.nextInt(map.h), put );
 			}
-			map.put( r.nextInt(map.w), r.nextInt(map.h), put );
+		} else {
+			// Separate ants and receptacles
+			for( int i=0; i<128; ++i ) {
+				map.put( r.nextInt(map.w), r.nextInt(map.h), ANT );
+			}
+			for( int i=0; i<24; ++i ) {
+				int cx = r.nextInt(map.w);
+				int cy = r.nextInt(map.h);
+				for( int dy=0; dy<3; ++dy ) for( int dx=0; dx<3; ++dx ) {
+					map.put(cx+dx, cy+dy, FEEDEE);
+				}
+			}
 		}
 		
-		for( int i=0; i<64; ++i ) {
-			int cx = r.nextInt(map.w);
-			int cy = r.nextInt(map.h);
-			for( int dy=0; dy<5; ++dy ) for( int dx=0; dx<5; ++dx ) {
-				map.put(cx+dx, cy+dy, FOOD);
+		if( (seed % 3) == 1 ) {
+			// Fat food
+			for( int i=0; i<24; ++i ) {
+				int cx = r.nextInt(map.w);
+				int cy = r.nextInt(map.h);
+				for( int dy=0; dy<7; ++dy ) for( int dx=0; dx<7; ++dx ) {
+					map.put(cx+dx, cy+dy, FOOD);
+				}
+			}
+		} else {
+			for( int i=0; i<64; ++i ) {
+				int cx = r.nextInt(map.w);
+				int cy = r.nextInt(map.h);
+				for( int dy=0; dy<5; ++dy ) for( int dx=0; dx<5; ++dx ) {
+					map.put(cx+dx, cy+dy, FOOD);
+				}
 			}
 		}
 	}
@@ -248,9 +291,13 @@ public class Ants
 	}
 	
 	public void runInWindow() throws Exception {
+		boolean writeImages = true;
+		int scale = writeImages ? 1 : 4;
+		
 		final Frame f = new Frame("PPP1");
 		final ImageCanvas c = new ImageCanvas();
-		c.setPreferredSize( new Dimension(640, 360) );
+		c.setBackground(Color.BLACK);
+		c.setPreferredSize( new Dimension(map.w*scale, map.h*scale) );
 		f.add(c);
 		f.pack();
 		f.addWindowListener( new WindowAdapter() {
@@ -260,14 +307,31 @@ public class Ants
 		});
 		f.setVisible(true);
 		
-		initMap();
+		long runId = System.currentTimeMillis();
+		File runDir = new File(String.format("/home/stevens/incoming/images/Vizations/output/run-%d",runId));
 		
-		BufferedImage img = new BufferedImage(640, 360, BufferedImage.TYPE_INT_ARGB);
-		for( int frame=0; frame<Integer.MAX_VALUE; ++frame ) {
-			draw(map, img.getGraphics());
-			c.setImage(img);
-			updateMap();
-			Thread.sleep(10);
+		Color successOverlayColor = new Color(1,1,1,0.5f);
+		BufferedImage img = new BufferedImage(map.w*scale, map.h*scale, BufferedImage.TYPE_INT_ARGB);
+		int setCount = writeImages ? 7 : Integer.MAX_VALUE;
+		for( int set=0; set<setCount; ++set ) {
+			initMap(set);
+			feedings = 0;
+			
+			File setDir = new File( runDir, String.format("set-%04d", set) );
+			if( writeImages ) setDir.mkdirs();
+			
+			for( int frame=0; frame<1024; ++frame ) {
+				synchronized(img) {
+					Graphics g = img.getGraphics();
+					draw(map, g, scale);
+					g.setColor(successOverlayColor);
+					g.fillRect(0, 0, feedings*scale/4, 2*scale);
+					c.setImage(img);
+					if( writeImages ) ImageIO.write(img, "png", new File(setDir, String.format("f%08d.png", frame)));
+				}
+				if( !writeImages ) Thread.sleep(10);
+				updateMap();
+			}
 		}
 	}
 	
